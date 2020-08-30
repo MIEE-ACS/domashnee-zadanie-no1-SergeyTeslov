6e6c9e7c19055eda8bea49ea4665c604e87f700f
SergeyTeslov <31865354+SergeyTeslov@users.noreply.github.com>
2020-08-30 12:53:03 +0300

    домашняя работа 1 сделана
    вариант 29

---
+++ dz1/dz1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
